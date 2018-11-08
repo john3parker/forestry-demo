cb19e176553b4e2c26449e7f0582e14543aeebb8
John Parker <john.parker@scoutmanager.com>
2018-11-07 19:16:20 -0600

    Moved script to .js file

---
+++ js/fjm.stripe.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+   $('#btnDonate').click(function() {
+		vex.dialog.open({
+		    message: 'Thank you for your donation!',
+		    input: [
+		        '<input name="amount" type="number" placeholder="Donation amount" required />',
+		    ].join(''),
+		    buttons: [
+		        $.extend({}, vex.dialog.buttons.YES, { text: 'Donate' }),
+		        $.extend({}, vex.dialog.buttons.NO, { text: 'Back' })
+		    ],
+		    callback: function (data) {
+		        if (!data) {
+		            //console.log('Cancelled')
+		        } else {
+		            //console.log('Amount', data.amount);
+		            data.amount = data.amount * 100;
+		            $('input#amount').val(data.amount);
+		            
+		            if (data.amount < 500) {
+		            	vex.dialog.alert('The minimum donation amount is $5.00');
+		            	return;
+		            }
+		            var handler = StripeCheckout.configure({
+		            	key: 'pk_live_QUqhIKbjYN5t9kYRVYWWHnHW'
+		            	,locale: 'auto'
+		            	,name: 'FJ Ministries'
+		            	,description: 'One-time donation'
+		            	,amount: data.amount
+		            	,token: function(token) {
+		            		$('input#stripeToken').val(token.id);
+		            		$('input#amount').val(data.amount);
+		            		//$('form#manualStripe').submit();
+		            		stripePay(token.id, data.amount);
+		            	}
+		            });
+		            handler.open({amount: Math.round(amount)});
+		        }
+		    }
+		});
+   });
+ });
+ function stripePay(stripeToken, amount) {
+ 	var data = {};
+ 	data.stripeToken = stripeToken;
+ 	data.amount = amount;
+ 	
+ 	$.post('https://pmvruqtzuf.execute-api.us-east-1.amazonaws.com/prod/', data, function(result) {
+ 		if (result.err) {
+ 			vex.dialog.alert('There was a problem processing with Stripe. Please contact FJ Ministries.');
+ 		}
+ 		else {
+ 			vex.dialog.alert('Stripe processing is complete. Thank you for your donation!')
+ 		}
+ 	}).fail(function(result) {
+ 		console.log(result);
+		vex.dialog.alert('There was a problem processing with Stripe. Please contact FJ Ministries.');
+ 	})
+ }
+ 
